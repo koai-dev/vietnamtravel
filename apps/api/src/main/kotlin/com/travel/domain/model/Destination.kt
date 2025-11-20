196e756eb8585c4ef1a9db59f03bd8f618ad7017
Nguyễn Kim Khánh <v.khanhnk3@vinsmartfuture.tech>
2025-11-21 01:26:03 +0700

    refactor

--- apps/api/src/main/kotlin/com/travel/domain/model/Destination.kt
+++ apps/api/src/main/kotlin/com/travel/domain/model/Destination.kt
@@ -1,8 +1,11 @@
 package com.travel.domain.model
 
 import aws.smithy.kotlin.runtime.content.BigDecimal
+import com.travel.core.BigDecimalSerializer
 import com.travel.data.table.DestinationType
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Destination(
     val id: Long = 0,
     val nameVi: String,
@@ -24,6 +27,7 @@ data class Destination(
     val status: String?,
 )
 
+@Serializable
 data class DestinationDetail(
     val id: Long = 0,
     val nameVi: String,
@@ -44,7 +48,9 @@ data class DestinationDetail(
     val tags: List<String>,
     val bestTimeToVisit: String?,
     val openingHours: String?,
+    @Serializable(with = BigDecimalSerializer::class)
     val priceFrom: BigDecimal?,
+    @Serializable(with = BigDecimalSerializer::class)
     val priceTo: BigDecimal?,
     val externalLinks: List<String>,
     val addressLink: String?,
--- apps/api/src/main/kotlin/com/travel/domain/model/Restaurant.kt
+++ apps/api/src/main/kotlin/com/travel/domain/model/Restaurant.kt
@@ -1,5 +1,8 @@
 package com.travel.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Restaurant(
     val id: Long,
     val name: String,
